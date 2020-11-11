5ae1e38b953d46a0e12a36814550d5e7b69150a4
Sergey <chechelsk@gmail.com>
2020-11-11 08:30:01 +0200

    use fetch

--- js/script.js
+++ js/script.js
@@ -1,35 +1,52 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     function req() {
-        const request = new XMLHttpRequest();
-        request.open('GET', 'http://localhost:3000/people');
-        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-        request.send();
+        // const request = new XMLHttpRequest();
+        // request.open('GET', 'http://localhost:3000/people');
+        // request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        // request.send();
 
-        request.addEventListener('load', function() {
-            if (request.readyState === 4 && request.status == 200) {
-                let data = JSON.parse(request.response);
+        // request.addEventListener('load', function() {
+        //     if (request.readyState === 4 && request.status == 200) {
+        //         let data = JSON.parse(request.response);
+        //         createCards(data);
+        //     } else {
+        //         console.error('Что-то пошло не так...');
+        //     }
+        // });
 
-                data.forEach(item => {
-                    let card = document.createElement('div');
-                    card.classList.add('card');
-                    card.innerHTML = `
-                        <img src="${item.photo}" alt="photo">
-                        <div class="name">${item.name} ${item.surname}</div>
-                        <div class="sex">
-                            <img src="${item.sex == 'male' ? 'icons/mars.png' : 'icons/female.png'}" alt="male">
-                        </div>
-                        <div class="age">${item.age}</div>
-                    `;
+        getResource('http://localhost:3000/people')
+            .then(data => createCards(data))
+            .catch(err => console.error(err));
+
+            
+    }
+
+    async function getResource(url) {
+        const res = await fetch(`${url}`);
+
+        if (!res.ok) {
+            throw new Error(`Could not fatch ${url}, status: ${res.status}`);
+        }
+        return await res.json();
+    }
+
     
-                    document.querySelector('.app').append(card);
-                });
+    function createCards(data) {
+        data.forEach(item => {
+            let card = document.createElement('div');
+            card.classList.add('card');
+            card.innerHTML = `
+                <img src="${item.photo}" alt="photo">
+                <div class="name">${item.name} ${item.surname}</div>
+                <div class="sex">
+                    <img src="${item.sex == 'male' ? 'icons/mars.png' : 'icons/female.png'}" alt="male">
+                </div>
+                <div class="age">${item.age}</div>
+            `;
 
-            } else {
-                console.error('Что-то пошло не так...');
-            }
+            document.querySelector('.app').append(card);
         });
-
     }
 
     const btn = document.querySelector('.cards');
@@ -47,6 +64,8 @@ window.addEventListener('DOMContentLoaded', () => {
         }
         
     });
+
+    
     
 
 
