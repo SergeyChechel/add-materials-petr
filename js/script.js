5e38f2106f3c4efb48cdf78b99a024f3d88c8557
Sergey <chechelsk@gmail.com>
2020-11-11 14:21:44 +0200

    fetch-POST-FormData

--- js/script.js
+++ js/script.js
@@ -24,43 +24,46 @@ window.addEventListener('DOMContentLoaded', () => {
         // let json = JSON.stringify(obj);
 
 
-        const request = new XMLHttpRequest();
-        request.open('POST', 'api.php');
-        // request.setRequestHeader('Content-type', 'multipart/form-data');
-        request.send(formData);
+        // const request = new XMLHttpRequest();
+        // request.open('POST', 'api.php');
+        // // request.setRequestHeader('Content-type', 'multipart/form-data');
+        // request.send(formData);
 
-        request.addEventListener('load', function() {
-            if (request.readyState === 4 && request.status == 200) {
-                // let data = JSON.parse(request.response);
-                console.log(request.response);
-                // createCards(data);
-            } else {
-                console.error('Что-то пошло не так...');
-            }
-        });
+        // request.addEventListener('load', function() {
+        //     if (request.readyState === 4 && request.status == 200) {
+        //         // let data = JSON.parse(request.response);
+        //         console.log(request.response);
+        //         // createCards(data);
+        //     } else {
+        //         console.error('Что-то пошло не так...');
+        //     }
+        // });
 
-        // getResource('http://localhost:3000/people')
-        //     .then(data => createCards(data.data))
-        //     .catch(err => console.error(err));
+        getResource('api.php', formData)
+            .then(data => console.log(data))
+            .catch(err => console.error(err));
 
             
     }
 
-    // async function getResource(url) {
-    //     const res = await fetch(`${url}`);
-    //     if (!res.ok) {
-    //         throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-    //     }
-    //     return await res.json();
-    // }
-
-    async function getResource(url) {
-        const res = await axios(`${url}`);
-        if (res.status !== 200) {
+    async function getResource(url, data) {
+        const res = await fetch(`${url}`, {
+            method: 'POST',
+            body: data
+        });
+        if (!res.ok) {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
-        return res;
+        return await res.text();
     }
+
+    // async function getResource(url) {
+    //     const res = await axios(`${url}`);
+    //     if (res.status !== 200) {
+    //         throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    //     }
+    //     return res;
+    // }
 
     
     function createCards(data) {
